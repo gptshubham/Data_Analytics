580d12d16f6fe9f343d22d22c054ca819756e8f4
gptshubham <shubhamca76@gmail.com>
2024-05-09 17:58:04 +0530

    MySQL cross joins and Analytics on Table completed

--- CodeBasics-MySQL-GitHub/Scripts/ch3-SQL_Basics-Data_Retrieval-Multiple_Tables.sql
+++ CodeBasics-MySQL-GitHub/Scripts/ch3-SQL_Basics-Data_Retrieval-Multiple_Tables.sql
@@ -1,3 +1,5 @@
+-- SQL Joins
+/*
 -- INNER JOIN
 SELECT 
 	m.movie_id, title, budget, revenue, currency, unit
@@ -49,4 +51,67 @@ FROM movies
 LEFT JOIN financials
 USING (movie_id);
 -- similarly we can do Right Join and Full Join also with USING clause
+*/
 
+-- Cross Joins
+/*
+SELECT * FROM food_db.items;
+SELECT * FROM food_db.variants;
+
+SELECT *
+FROM food_db.items
+CROSS JOIN food_db.variants;
+
+SELECT COUNT(*)
+FROM food_db.items
+CROSS JOIN food_db.variants;
+
+SELECT
+	food_db.items.name,
+    food_db.variants.variant_name,
+    (food_db.items.price + food_db.variants.variant_price) as price
+FROM food_db.items
+CROSS JOIN food_db.variants
+ORDER BY food_db.variants.variant_name DESC;
+
+SELECT
+	*,
+    concat(name," - ",variant_name) as full_name,
+    (food_db.items.price + food_db.variants.variant_price) as final_price
+FROM food_db.items
+CROSS JOIN food_db.variants;
+
+SELECT
+    concat(name," - ",variant_name) as item,
+    (food_db.items.price + food_db.variants.variant_price) as price
+FROM food_db.items
+CROSS JOIN food_db.variants;
+*/
+
+-- Analytics on Tables
+
+SELECT 
+	m.movie_id,
+    title,
+    budget,
+    revenue,
+    currency,
+    unit,
+    (revenue - budget) as profit
+FROM movies m
+JOIN financials f
+ON m.movie_id=f.movie_id;
+
+-- above table for bollywood movies only (movies with larger profit comes first)
+SELECT 
+	m.movie_id, title, budget, revenue, currency, unit,
+    CASE
+		WHEN unit="thousands" THEN ROUND((revenue-budget)/1000)
+        WHEN unit="billions" THEN ROUND((revenue-budget)*1000)
+        ELSE ROUND((revenue-budget))
+    END as profit_mln
+FROM movies m
+JOIN financials f
+ON m.movie_id=f.movie_id
+WHERE industry="bollywood"
+ORDER BY profit_mln DESC;
