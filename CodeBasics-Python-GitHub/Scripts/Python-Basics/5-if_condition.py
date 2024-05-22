e39dde988d012e9b45621397004dd7ac3eaeef6c
gptshubham <shubhamca76@gmail.com>
2024-05-22 19:11:02 +0530

    if condition completed

---
+++ CodeBasics-Python-GitHub/Scripts/Python-Basics/5-if_condition.py
@@ -0,0 +1,32 @@
+# Even / Odd
+'''
+n = int(input('Enter a Number : '))
+print(n)
+print(type(n))
+
+if n%2 == 0:
+    print(f'{n} is Even!')
+else:
+    print(f'{n} is Odd!')
+
+# Alternatively using ternary Operator
+print(f'{n} is Even!' if n%2 ==0 else print(f'{n} is Odd!'))
+'''
+
+# Cuisine Program
+'''
+indian = ['samosa','daal','naan']
+chinese = ['egg role','pot sticker','fried rice']
+italian = ['pizza','pasta','risotto']
+
+dish = input("Enter a dish name:")
+if dish in indian:
+    print(f'{dish} is Indian!')
+elif dish in chinese:
+    print(f'{dish} is Chinese!')
+elif dish in italian:
+    print(f'{dish} is Italian!')
+else:
+    print("I don't know which cuisine is this!")
+'''
+
